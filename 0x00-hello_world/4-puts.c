3f46eb5e0f3bccb4f9f2b313ffc73f9d418224f2
Ebenezerterabo <teraboebenezer@gmail.com>
2023-02-17 04:31:47 +0000

    Hello.puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - programming is like building a multiligual puzzle
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multiligual puzzle");
+	
+	return (0);
+}
