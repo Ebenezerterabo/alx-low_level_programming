3ab051245b55c15cc1cd162b9ecc7d4c9971fa8b
Ebenezerterabo <teraboebenezer@gmail.com>
2023-02-19 00:26:23 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - programming is like building a multiligual puzzle
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"programming is like building a multiligual puzzle");
-	
+	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
 }
