5d70f0452e0e777971fc98ab1ef1d1c1b35fe283
Ebenezerterabo <teraboebenezer@gmail.com>
2023-02-23 04:40:52 +0000

    98 battery street, the 0G

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *add - A function that adds two integers and returns the result.
+ *@r: int to be added to r
+ *@s: int to be added to s
+ *Return: Always q (Success)
+ */
+
+int add(int r, int s)
+{
+	int q;
+
+	q = r + s;
+
+	return (q);
+
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - A function that prints all natural numbers from n
+ *to 98
+ *@n:int n added
+ *Return: 0 Always
+ */
+
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", a);
+			}
+
+		}
+
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (b != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+
+			}
+		}
+
+	}
+
+}
