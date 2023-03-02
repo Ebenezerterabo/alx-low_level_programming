7c9b516480fe65b1d7132d7a204a3f6fec81e61d
Ebenezerterabo <teraboebenezer@gmail.com>
2023-03-02 12:39:00 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings
+ *
+ * @s1: character 1
+ * @s2: character 2
+ *
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	for (c = 0; s1[c] != '\0' && s2[c] != '\0'; c++)
+	{
+		if (s1[c] != s2[c])
+
+			return (s1[c] != s2[c]);
+
+	}
+
+	return (0);
+
+}
