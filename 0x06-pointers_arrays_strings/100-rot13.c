4ddf7b7733bc0520842d722708ae8e5f7cfaaf5c
Ebenezerterabo <teraboebenezer@gmail.com>
2023-03-04 02:37:23 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes a string into rot13
+ *
+ *@str: string to encode
+ *Return: str as address
+ */
+
+char *rot13(char *str)
+{
+	int q, r;
+
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+
+	for (q = 0; *(str + q); q++)
+	{
+		for (r = 0; r < 52; r++)
+		{
+			if (a[r] == *(str + q))
+			{
+				*(str + q) = b[r];
+
+				break;
+
+			}
+
+		}
+
+	}
+
+	return (str);
+
+}
+
