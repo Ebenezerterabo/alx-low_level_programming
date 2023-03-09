6f86b525b9968ea1e745de52d4ac162737c94915
Ebenezer terabo <teraboebenezer@gmail.com>
2023-03-09 14:55:11 +0000

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural square
- * root of a number
+ * checker - checks for the square root
+ * @x : integer 1
+ * @y : integer 2
+ *
+ * Return: the natural square root of a number
+ */
+
+int checker(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+
+	if (x * x > b)
+		return (-1);
+
+	return (checker(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - A function that returns the natural square
+ * root of a number.
  * @n: integer value
  *
- * Return: natural square root of a number.
+ * Return: natural square root or -1
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (check_recursion(1, n));
+	return (checker(1, n));
 }
-
-
-/**
- * check_recursion - recurses to find the natural
- * square root of a number
- * @x: number to calculate the sqaure root of
- * @y: iterator
- *
- * Return: the resulting square root
- */
-int check_recursion(int x, int y)
-{
-	if (y * y > x)
-		return (-1);
-	if (y * y == x)
-		return (y);
-	return (check_recursion(x, y + 1));
-}
-
