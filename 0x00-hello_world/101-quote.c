ddfa430966526ac44e4a3449779c3f16c4422908
Ebenezer terabo <teraboebenezer@gmail.com>
2023-10-28 10:40:54 +0000

    Task 8 solved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,8 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	char *string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, string, strlen(string));
 	return (1);
 }
