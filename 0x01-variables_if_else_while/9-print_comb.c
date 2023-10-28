e06cbfde752b11a865916af3bc2d4dd0a2298456
Ebenezer terabo <teraboebenezer@gmail.com>
2023-10-28 17:24:43 +0000

    Task 9 solved

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		putchar(count + '0');
+
+		if (count < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		count++;
+	}
+
+	return (0);
+}
