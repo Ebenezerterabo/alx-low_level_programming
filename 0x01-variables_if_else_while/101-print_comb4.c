a49849fc69a158ffec8e3eb422b0ec6b3318ea91
Ebenezer terabo <teraboebenezer@gmail.com>
2023-10-29 20:56:16 +0000

    Task 11 solved

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible different combination of
+ * two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+			if (j != 8 || i != 7 || k != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			}
+
+			j++;
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
