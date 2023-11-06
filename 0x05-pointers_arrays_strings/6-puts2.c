eeb24cc3e760bd984b5422b8dc02fa2923583043
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-06 18:03:26 +0000

    Task 6 solved

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,16 @@
 
 void puts2(char *str)
 {
-	while (*str)
+	int i = 0;
+
+	while (str[i])
 	{
-		if (*str % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(*str);
+			_putchar(str[i]);
 		}
 
-		str++;
+		i++;
 	}
 
 	_putchar('\n');
