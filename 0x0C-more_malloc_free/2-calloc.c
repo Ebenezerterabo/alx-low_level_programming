f8375c6182fb510f3bfc1d70c9ede64b79741c63
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-24 00:30:40 +0000

    Task 2 solved

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *char_ptr;
 
-	if (size == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 
 	ptr = malloc(size * nmemb);
