7cc6ef400e0034d1d5aa78583300b020cd873e6c
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-28 16:45:44 +0000

    Task 0 solved

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure with dog details
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
