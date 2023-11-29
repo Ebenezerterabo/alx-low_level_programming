627bb31d517d7fdd451e18170dee768c0099b0ac
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-29 21:25:12 +0000

    Task 0 solved

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - A function that prints a name
  * @name: name
@@ -10,7 +10,7 @@
 
 void print_name(char *name, void(*f)(char *))
 {
-	if (name == '\0' || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
 	f(name);
