dad5f834ea688c339b60513f57aed162ccb21f5d
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-29 00:45:16 +0000

    Task 4 solved

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
+/**
+ * _strlen - A function to get the length of a string
+ * @str: the pointer to the first character of the string
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
 /**
  * new_dog - A function that creates a new dog
  * @name: the name of the dog
@@ -12,14 +33,42 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len_name, len_owner, i;
+
 	dog_t *ptr = malloc(sizeof(dog_t));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->name = name;
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
+
+	ptr->name = malloc(sizeof(char) * len_name + 1);
+
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr->owner = malloc(sizeof(char) * len_owner + 1);
+
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; name[i] != '\0'; i++)
+		ptr->name[i] = name[i];
+	ptr->name[i] = '\0';
+
+	for (i = 0; owner[i] != '\0'; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner[i] = '\0';
+
 	ptr->age = age;
-	ptr->owner = owner;
 
 	return (ptr);
 }
