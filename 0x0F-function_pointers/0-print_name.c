66950eff9519a5a29f719d7c1ba6bf3b37de05f0
Ebenezer terabo <teraboebenezer@gmail.com>
2023-11-29 21:16:34 +0000

    Task 0 solved

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ * @name: name
+ * @f: the function pointer
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	f(name);
+}
