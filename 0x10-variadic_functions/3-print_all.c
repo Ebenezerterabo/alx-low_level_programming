e7dfb2187b9117928fab5c789e6e0b7cf2eb0ba7
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-01 21:09:24 +0000

    Task 3 solved

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,10 +13,10 @@ void print_string(va_list ap)
 {
 	char *s = va_arg(ap, char *);
 
-	if (s != NULL)
-		printf("%s", s);
+	if (s == NULL)
+		s = "(nil)";
 	else
-		printf("(nil)");
+		printf("%s", s);
 }
 /**
  * print_all - A function that prints anything
@@ -29,40 +29,42 @@ void print_all(const char * const format, ...)
 {
 	int not_found = 0;
 	const char *ptr = format;
-	int arg_count = 0;
 
 	va_list ap;
 
 	va_start(ap, format);
 
-	while (*ptr && arg_count < 9)
+	if (format)
 	{
-		not_found = 0;
-
-		switch (*ptr)
+		while (*ptr)
 		{
-		case 'c':
-			printf("%c", va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			break;
-		case 's':
-			 print_string(ap);
-			break;
-		default:
-			not_found = 1;
-			break;
-		}
+			not_found = 0;
 
-		if (!not_found && *(ptr + 1))
-			printf(", ");
-		ptr++;
-		arg_count++;
+			switch (*ptr)
+			{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				print_string(ap);
+				break;
+			default:
+				not_found = 1;
+				break;
+			}
+
+			if (!not_found && *(ptr + 1))
+				printf(", ");
+			ptr++;
+			not_found = 0;
+		}
+		printf("\n");
+		va_end(ap);
 	}
-	printf("\n");
-	va_end(ap);
 }
