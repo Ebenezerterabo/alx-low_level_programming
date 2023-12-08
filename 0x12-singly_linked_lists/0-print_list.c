31439fa24369280744c92d1f9688434ec1ba3b74
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-08 23:51:04 +0000

    Task 0 solved

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,6 @@ size_t print_list(const list_t *h)
 	const list_t *current = h;
 	int element = 0;
 
-	if (current == NULL)
-		return (-1);
-
 	while (current != NULL)
 	{
 		if (current->str == NULL)
