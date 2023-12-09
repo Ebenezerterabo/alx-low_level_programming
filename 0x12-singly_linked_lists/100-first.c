3d182431d89b92096253874fdaee67fa8e0cb19c
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-09 03:00:31 +0000

    Advance task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 
 void __attribute__((constructor)) first()
 {
-	printf("You're best! and yet, you must allow");
+	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
