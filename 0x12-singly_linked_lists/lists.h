a99fbfd47ee3bedad80ec164e9c5a4a5f78aa7db
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-09 01:16:35 +0000

    Header file updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,10 +22,12 @@ typedef struct list_s
 /* Standard library */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Function Prototypes */
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
