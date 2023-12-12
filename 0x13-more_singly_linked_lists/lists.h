b1c8305c071108985c777a1b0a9314fc69a0eb87
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-12 13:55:20 +0000

    Header file updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 /* Standard Library */
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -23,5 +24,6 @@ typedef struct listint_s
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LIST_H */
