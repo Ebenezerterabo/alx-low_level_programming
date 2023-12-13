3304faad556c9c4e0701a3d2f1f44ebbccf99460
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-13 01:45:19 +0000

    Task 9 solved

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * node_len - A function that calculates the number of nodes
+ * in a linked list
+ * @head: the pointer to the head node
+ *
+ * Return: the length of the node
+ */
+
+int node_len(listint_t *head)
+{
+	listint_t *current = head;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * insert_nodeint_at_index - A function that inserts a node
  * at a given position
  * @head: a pointer to the head node
@@ -12,23 +34,28 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode, *temp, *current;
-	unsigned int count = 0, i = 0;
+	listint_t *newNode, *temp;
+	unsigned int i = 0, len_node;
+
+	if (head == NULL)
+		return (NULL);
 
 	newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 		return (NULL);
 
-	current = *head;
+	newNode->n = n;
+	len_node = node_len(*head);
 
-	while (current != NULL)
+	if (idx == 0 || *head == NULL)
 	{
-		current = current->next;
-		count++;
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
 
-	if (idx > count)
+	if (idx > len_node)
 		return (NULL);
 
 	temp = *head;
@@ -39,7 +66,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
-	newNode->n = n;
 	newNode->next = temp->next;
 	temp->next = newNode;
 
