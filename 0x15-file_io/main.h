e7cef2f15249cc4a1f599f71471f1972a51b1527
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-18 23:28:14 +0000

    Task 0 solved

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Library */
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+/* Function Prototype */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
