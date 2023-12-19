59e7b3b3af55e9a16e9645744eb43885c39ccb8f
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-19 00:36:07 +0000

    Header file updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /* Function Prototype */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 #endif
