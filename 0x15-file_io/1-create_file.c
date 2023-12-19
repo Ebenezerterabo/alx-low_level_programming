ebdee57546581bd4fed395d3b2d4de3e758d9d0a
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-19 00:49:55 +0000

    Task 1 solved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_dpt = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	file_dpt = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file_dpt == -1)
 		return (-1);
@@ -29,10 +29,7 @@ int create_file(const char *filename, char *text_content)
 	len_write = write(file_dpt, text_content, len);
 
 	if (len_write == -1)
-	{
 		close(file_dpt);
-		return (-1);
-	}
 
 	close(file_dpt);
 	return (1);
