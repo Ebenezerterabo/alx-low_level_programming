4fe456da075c04fec21e4926e3df83cee195bec5
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-20 02:15:28 +0000

    Header file updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#define BUFSIZE 1024
 /* Standard Library */
 #include <stdio.h>
 #include <unistd.h>
@@ -10,9 +10,9 @@
 
 
 /* Function Prototype */
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copyFile(const char *file_from, const char *file_to);
 
 #endif
