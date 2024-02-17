ba8ec06efd4ed23504adb54072c0ada3c4a1f63d
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-17 02:35:36 +0000

    Task 3 solved

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = hash_djb2((unsigned char *)key);
+	unsigned long int idx;
 	hash_node_t *newNode, *current;
 
 	if (!ht || !key || *key == '\0')
