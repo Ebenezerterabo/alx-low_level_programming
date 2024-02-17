cab99e9cc64ac02b877b6a4955d0e8fe032bafcf
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-17 02:37:12 +0000

    Task 3 solved

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current->value);
 			current->value = strdup(value);
-			if (current->value)
+			if (current->value == NULL)
 				return (0);
 			return (1);
 		}
