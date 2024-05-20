7f5ea1d45b010f02b09cef5c9ad0bfdac24d1cf1
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-20 18:05:00 +0000

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Standard Library */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function Prototypes */
+int linear_search(int *array, size_t size, int value);
+#endif
