44f1509a61f09282ff40fcf83905e6e56c5c7422
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-20 18:08:31 +0000

    Task 0 solved

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,6 +15,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i;
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
@@ -23,6 +26,6 @@ int linear_search(int *array, size_t size, int value)
 			return (i);
 		}
 	}
-	
+
 	return (-1);
 }
