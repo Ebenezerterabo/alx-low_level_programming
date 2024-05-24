491dec512d4c49d4fe948374842349a134b2df61
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-24 02:17:37 +0000

    Header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,9 @@
 /* Standard Library */
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
